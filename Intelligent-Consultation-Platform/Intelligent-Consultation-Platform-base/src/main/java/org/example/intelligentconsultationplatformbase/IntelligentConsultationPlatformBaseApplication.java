a474c2b3e8b87be8e02180276734676591cf7fa1
Rinnao <160581799+RinnaoOKW@users.noreply.github.com>
2026-03-29 17:28:30 +0800

    Add files via upload
    sql upload and I regroup our basement

---
+++ Intelligent-Consultation-Platform/Intelligent-Consultation-Platform-base/src/main/java/org/example/intelligentconsultationplatformbase/IntelligentConsultationPlatformBaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.intelligentconsultationplatformbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentConsultationPlatformBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentConsultationPlatformBaseApplication.class, args);
+	}
+
+}
